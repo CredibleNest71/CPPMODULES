e41a5942da7d845252c463a63b74138ed84e1976
CredibleNest71 <moritz.res@gmail.com>
2025-04-22 16:57:27 +0200

    FJ works

--- CPP09/ex02/PM.hpp
+++ CPP09/ex02/PM.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mresch <mresch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/28 11:05:18 by mresch            #+#    #+#             */
-/*   Updated: 2025/04/16 12:34:26 by mresch           ###   ########.fr       */
+/*   Created: 2025/04/16 13:28:03 by mresch            #+#    #+#             */
+/*   Updated: 2025/04/22 16:50:48 by mresch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,158 @@
 # include <sstream> // Added for std::istringstream
 # include <utility> // Added for std::pair
 
-class PM {
-public:
-    // Canonical form
-    PM();
-    PM(const PM& other);
-    ~PM();
-    PM& operator=(const PM& other);
+extern int global;
 
-    int readInput(const std::string& input) ;
-    std::vector<int> jacobshal(int upper);
+template<typename T>
+void printVec(T& v){
+    for (size_t i = 0; i < v.size(); i++){
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+template<typename NestedContainer>
+void printVecVec(NestedContainer& v){
+    for (size_t i = 0; i < v.size(); i++){
+        printVec(v[i]);
+    }
+}
+
+template<typename Container>
+Container jacobsthal(int upper){
+    Container nums;
+    nums.push_back(0);
+    nums.push_back(1);
+    while(nums.back() < upper){
+        int next = (nums[nums.size()-1] + 2 * nums[nums.size()-2]);
+        nums.push_back(next);
+    }
+    nums.erase(nums.begin() + 1);
+    return nums;
+}
+
+template<typename NestedContainer>
+int binaryInsertLeftover(NestedContainer&bigger, typename NestedContainer::value_type leftover){
+    int left = 0;
+    int right = bigger.size() - 1;
+    int value = leftover.front();
     
-    int fordjohnson(std::vector<int>& arr);
-    int binaryInsert(std::vector<int>& arr, int value, std::vector<std::pair<int, int> >& pairs);
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (bigger[mid].front() < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        global++;
+    }
+    bigger.insert(bigger.begin() + left, leftover);
+    return left;
+}
 
-    int binaryInsertD(std::deque<int>& arr, int value, std::deque<std::pair<int, int> >& pairs);
-    int fordjohnsonD(std::deque<int>& arr);
+template<typename NestedContainer>
+int binaryInsert(NestedContainer &bigger, NestedContainer&smaller, int idx, typename NestedContainer::value_type &idxes){
+    int left = 0;
+    int right = idxes[idx];
+    int value = smaller[idx].front();
     
-    void printVector(std::vector<int>& v, int limit);
-    void printDeque(std::deque<int>& d, int limit);
-            
-            
-    std::vector<int> _v;
-    std::deque<int> _d;
-};
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (bigger[mid].front() < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        global++;
+    }
+    bigger.insert(bigger.begin() + left, smaller[idx]);
+    for (size_t i = 0; i < idxes.size(); i++){
+        idxes[i]++;
+    }
+    return left;
+}
 
-#endif 
+template<typename NestedContainer>
+int fordjohnson(NestedContainer& arr) {
+    typedef typename NestedContainer::value_type value_type;
+    int n = arr.size();
+    if (n <= 1)
+        return 0;
+
+    NestedContainer merge;
+    // Make pairs
+    for (size_t i = 0; i + 1 < arr.size(); i += 2) {
+        value_type temp;
+        if (arr[i].front() > arr[i + 1].front()) {
+            global++;
+            temp.insert(temp.end(), arr[i].begin(), arr[i].end());
+            temp.insert(temp.end(), arr[i + 1].begin(), arr[i + 1].end());
+        } else {
+            temp.insert(temp.end(), arr[i + 1].begin(), arr[i + 1].end());
+            temp.insert(temp.end(), arr[i].begin(), arr[i].end());
+        }
+        merge.push_back(temp);
+    }
+
+    value_type leftover;
+    bool hasleftover = n % 2;
+    if (hasleftover){
+        leftover = arr[n - 1];
+    }
+    arr = merge;
+
+    fordjohnson(arr);
+
+    value_type jacob = jacobsthal<value_type>(arr.size());
+    NestedContainer bigger;
+    NestedContainer smaller;
+    value_type idxes;
+
+    // Split
+    NestedContainer split;
+    for (size_t i = 0; i < arr.size(); i++) {
+        value_type a, b;
+        for (size_t j = 0; j < arr[i].size(); j++) {
+            if (j < arr[i].size() / 2)
+                a.push_back(arr[i][j]);
+            else
+                b.push_back(arr[i][j]);
+        }
+        split.push_back(a);
+        split.push_back(b);
+    }
+
+    arr = split;
+
+    // Fill bigger/smaller vectors
+    for (size_t i = 0; i < arr.size(); i++) {
+        if (i % 2 == 0) {
+            bigger.push_back(arr[i]);
+            idxes.push_back(i / 2);
+        } else {
+            smaller.push_back(arr[i]);
+        }
+    }
+
+    int j = 0;
+    int idx = jacob[j];
+    while (!smaller[idx].empty()) {
+        binaryInsert(bigger, smaller, idx, idxes);
+        smaller[idx].clear();
+        idx--;
+        if (idx < 0 || smaller[idx].empty()) {
+            j++;
+            idx = jacob[j];
+            while (idx > (int)smaller.size() - 1) {
+                idx--;
+            }
+        }
+    }
+    if (hasleftover)
+        binaryInsertLeftover(bigger, leftover);
+
+    arr = bigger;
+    return 0;
+}
+
+#endif
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -5,46 +5,74 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mresch <mresch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/28 11:03:28 by mresch            #+#    #+#             */
-/*   Updated: 2025/04/16 12:55:47 by mresch           ###   ########.fr       */
+/*   Created: 2025/04/18 17:06:28 by mresch            #+#    #+#             */
+/*   Updated: 2025/04/22 16:43:05 by mresch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctime>
 #include "PM.hpp"
+#include <ctime>
 
+// int fordjohnson(std::vector<std::vector<int> >& arr);
+//void printVecVec(std::vector<std::vector<int> >& v);
+template<typename NestedContainer>
+bool sorted(NestedContainer& v){
+    int curr = 0;
+    bool ret = true;
+    for (size_t i = 0; i < v.size(); i++){
+        if (v[i].front() < curr){
+            std::cout << "Not sorted: " << v[i].front() << " < " << curr << std::endl;
+            ret = false;
+        }
+        curr = v[i].front();
+    }
+    if (ret)
+        std::cout << "Sorted!" << std::endl;
+    return ret;
+}
+
+int global = 0;
 
 int main(int ac, char **av) {
-    PM pm;
-    if (ac != 2) {
-        std::cerr << "Usage: " << av[0] << " <input>" << std::endl;
+    std::srand(static_cast<unsigned int>(std::time(0)));
+    
+    std::vector<std::vector<int> > arr;
+    std::deque<std::deque<int> > arrD;
+    int amount = 16;
+    if (ac >= 2)
+        amount = atoi(av[1]);
+    if (amount < 0){
+        std::cerr << "Error: Negative number: " << amount << std::endl;
         return 1;
     }
-    std::string input = av[1];
-    if (pm.readInput(input))
-        return 1;
+    for (int i = 0; i < amount; i++){
+        int num = std::rand() % amount * 2;
+        std::vector<int> temp;
+        temp.push_back(num);
+        std::deque<int> tempD;
+        tempD.push_back(num);
+        arr.push_back(temp);
+        arrD.push_back(tempD);
+    }
 
-    std::cout << "Before: ";
-    pm.printVector(pm._v, 5);
-
-    // Measure execution time of fordjohnson with VECTOR
     std::clock_t startV = std::clock();
-    pm.fordjohnson(pm._v);
+    fordjohnson(arr);
+    int compV = global;
+    global = 0;
     std::clock_t endV = std::clock();
     double elapsed_time_vector = static_cast<double>(endV - startV) / CLOCKS_PER_SEC;
     
     // Measure execution time of fordjohnson with DEQUE
     std::clock_t startD = std::clock();
-    pm.fordjohnsonD(pm._d);
+    fordjohnson(arrD);
+    int compD = global;
     std::clock_t endD = std::clock();
     double elapsed_time_deque = static_cast<double>(endD - startD) / CLOCKS_PER_SEC;
     
-    std::cout << "After: ";
-    pm.printVector(pm._v, 5);
-    std::cout << "Time to process a range of " << pm._v.size() << " elements with std::vector : " << elapsed_time_vector << " seconds" << std::endl;
-    std::cout << "Time to process a range of " << pm._d.size() << " elements with std::deque : " << elapsed_time_deque << " seconds" << std::endl;
-
-
-
-    return 0; 
+    std::cout << "After: " << std::endl;
+    printVecVec(arr);
+    sorted(arr);
+    std::cout << "Time to proess a range of " << arr.size() << " elements with std::vector : " << elapsed_time_vector << " seconds, with " << compV << " comparisons" << std::endl;
+    std::cout << "Time to process a range of " << arrD.size() << " elements with std::deque : " << elapsed_time_deque << " seconds, with " << compD << " comparisons" << std::endl;
+    return 0;
 }
